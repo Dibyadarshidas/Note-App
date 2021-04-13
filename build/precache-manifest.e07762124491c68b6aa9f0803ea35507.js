701d717ad9f2980d997af4e5783a1f9db0644ac4
Dibyadarshi Das <ddas270@gmail.com>
2021-04-14 01:41:02 +0530

    Build File

---
+++ build/precache-manifest.e07762124491c68b6aa9f0803ea35507.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41519f23260b4f7427fbc8f4f08feb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fa5ac507983a3b476d",
+    "url": "/static/js/2.b6ca354c.chunk.js"
+  },
+  {
+    "revision": "439065b912d7b8b2aed1",
+    "url": "/static/js/main.1a0cef9f.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
